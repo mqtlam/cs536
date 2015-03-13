b687d0d288d29ecc1cd6e8103a5cc6b4a2ff955e
collwe <collwe01@gmail.com>
2015-03-12 20:06:56 -0700

    Basic Framework of Mixture Tress
    Left EM to fill in

---
+++ MixTree/GenerateInitMT.m
@@ -0,0 +1,37 @@
+function [MT_edgeStructs,MT_nodePots,MT_edgePots] = GenerateInitMT( nNodes, nStates, m, fUV, option )
+
+% Input:    nNodes -> # of nodes 
+%           m      -> # of mixture trees
+%           fUV    -> pairwise frequency over all nodes
+%           option -> 1. generate the MI mat, then shuffle rows and cols
+%                     2. split the scene instances into groups, then build tree seperately
+% Output:   miXY_shuffled -> m*1 cell array stores the MI mat of 'm' trees
+
+MT_edgeStructs = cell(m,1);
+MT_nodePots = cell(m,1);
+MT_edgePots = cell(m,1);
+
+if option == 1
+    % generate the MI mat first
+    miXY = CalculateMutualInfoMatrix(fUV, nNodes);
+    % run maximum spanning tree algorithm on randomly shuffled MI matrix
+    for is = 1:1:m
+        ridx = randperm(nNodes);
+        cidx = randperm(nNodes);
+        miXY = miXY(ridx,:);
+        miXY = miXY(:,cidx);
+        mi_half = triu(miXY,1);
+        Aff = MaxSpan(mi_half + mi_half');
+        % graph struct
+        MT_edgeStructs{is,1} = UGM_makeEdgeStruct(Aff, nStates);
+        % graph node potential
+        MT_nodePots{is,1} = ones(nNodes, nStates);
+        % graph edge potentail
+        MT_edgePots{is,1} = GetEdgePot(MT_edgeStructs{is,1}, fUV, nStates);
+    end
+    
+else % option == 2
+    
+end
+
+
---
+++ MixTree/TrainMixTrees.m
@@ -0,0 +1,87 @@
+function [ edgeStructs, nodePots, edgePots ] = TrainMixTrees( foundTrainObjectsList, scenes, objectsVocab )
+%TRAINMIXTREE Train graphical model of mixture of Chow-Liu trees.
+%
+%   Each object node comes from counting presence/absence in images.
+%  
+%   foundTrainObjectsList:  list of object counts
+%                           Each cell is a containers.Map
+%                           Each map stores the objects found as keys
+%                               and the object counts as values
+%   scenes:                 map where keys are scenes and values are indices to examples
+%   objectsVocab:           list of objects vocabulary (from training and test sets)
+%   edgeStructs:            map where keys are scenes and values are
+%                               edgeStructs (UGM)
+%   nodePots:               map where keys are scenes and values are 
+%                               node potentials (UGM)
+%   edgePots:               map where keys are scenes and values are 
+%                               edge potentials (UGM)
+%
+
+%% initialization
+edgeStructs = containers.Map;
+nodePots = containers.Map;
+edgePots = containers.Map;
+
+nNodes = length(objectsVocab);
+nStates = 2;
+maxIter = 20; % maximum iteration for EM 
+
+%% for convenience: map object name to index
+objectsVocabMap = containers.Map;
+for i = 1:length(objectsVocab)
+    objectsVocabMap(objectsVocab{i}) = i;
+end
+
+%% loop through scenes
+for s = keys(scenes)
+    scene = s{1};
+
+    %% Initialize the MT model
+    % m is the # of mixture trees want to learn
+    m = 2;
+    MT_lambda = ones(m,1);
+    MT_lambda = MT_lambda/sum(MT_lambda); % initial lambda has uniform probability
+
+    %% randomly initialize the tree components
+    % preprocess
+    examples = scenes(scene);
+    foundObjectsList = cell(length(examples), 1);
+    for ex = 1:length(examples)
+        foundObjectsList{ex} = foundTrainObjectsList{examples(ex)};
+    end
+    samples = PreprocessData(foundObjectsList, objectsVocab);
+    
+    fUV = CalculateVariableStatesFreq(nStates, nNodes, samples);
+    
+    % Generate the Initial Mixture trees
+    [MT_edgeStructs,MT_nodePots,MT_edgePots] = GenerateInitMI( nNodes, nStates, m, fUV, 1);
+
+    %% Start the EM iterations
+    for it = 1:1:maxIter
+        % E step
+    
+        % M step
+        
+    end
+    
+    %% updates    
+    edgeStructs(scene) = MT_edgeStructs;
+    nodePots(scene) = MT_nodePots;
+    edgePots(scene) = MT_edgePots;
+end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
